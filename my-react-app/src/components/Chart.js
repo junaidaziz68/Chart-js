7e0a9f9a29c2b092380d477347fa25a31536cb7d
junaidaziz68 <junaidaziz68@outlook.com>
2022-11-28 19:47:03 +0000

    updated

--- my-react-app/src/components/Chart.js
+++ my-react-app/src/components/Chart.js
@@ -1,34 +1,33 @@
 import React, { useEffect, useState } from "react";
-import {Bar} from 'react-chartjs-2'
-import Chart from 'chart.js/auto';
-import * as d3 from "d3";
-import {csv} from 'd3'
+import {Line} from 'react-chartjs-2'
+import *  as d3 from "d3";
+import {csv} from "d3"
 
 
 
 
 function BarChart (){
  const [data ,setData]=useState([]);
-
-    useEffect(()=>{
-  csv('data.csv').then(data =>{
-
-    setData(data)
+ 
+ const [loading , setLoading]= React.useState(true);
+useEffect(()=>{
+  d3.csv('/data.csv').then(data =>{
+  
+  setData(data)
+  setLoading(false);
   })
     }, []);
 
 return (
 
 <div>
-<Bar
-    
-
-
-
- height={400}
+<Line data={data}
+  height={400}
  width = {600}
+ options ={options}
+ 
  />
-    
+     
 </div>
 
 
